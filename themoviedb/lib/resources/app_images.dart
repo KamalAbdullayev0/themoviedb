213e9c286adfacb8cf9e1a95409a75c17524939d
KamalAbdullayev0 <abdullayevkamal16.08@gmail.com>
2024-08-01 00:26:42 +0400

    3925

---
+++ themoviedb/lib/resources/app_images.dart
@@ -0,0 +1,7 @@
+abstract class AppImages {
+  static const String images1 = 'assets/images/tmbd_image1.jpg';
+  static const String images2 = 'assets/images/tmbd_image2.jpg';
+  static const String images3 = 'assets/images/tmbd_image3.jpg';
+  static const String images4 = 'assets/images/tmbd_image4.jpg';
+  static const String images5 = 'assets/images/tmbd_image5.jpg';
+}
